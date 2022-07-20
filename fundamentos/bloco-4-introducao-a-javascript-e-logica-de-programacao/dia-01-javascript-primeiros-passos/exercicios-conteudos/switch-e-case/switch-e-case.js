4ec4cdfeff7acfeac132236dd3baabc4f6de7a60
Wellington B <barbosadecarvalhow@gmail.com>
2022-07-20 19:17:18 -0300

    adiciona arquivo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-conteudos/switch-e-case/switch-e-case.js
@@ -0,0 +1,25 @@
+let mes = 'maio';
+let candidato = '?';
+
+switch (mes){  
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+    candidato = 'Verão'
+break;
+        case 'abril':
+        case 'maio':
+        case 'junhho':
+        candidato = 'Outono'
+break;
+        case 'julho':
+        case 'agosto':
+        case 'setembro':
+        candidato = 'Inverno'
+break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+    canditato = 'Primavera'
+}
+console.log (candidato)
