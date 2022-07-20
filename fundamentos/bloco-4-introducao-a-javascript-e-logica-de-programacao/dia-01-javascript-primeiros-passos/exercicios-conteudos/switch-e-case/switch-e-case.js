2dc2f8c0d05b79c7509e50df4cf41031d10adb5f
Wellington B <barbosadecarvalhow@gmail.com>
2022-07-20 19:23:33 -0300

    adiciona exercicio da página switch e case do course.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-conteudos/switch-e-case/switch-e-case.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-conteudos/switch-e-case/switch-e-case.js
@@ -1,25 +1,41 @@
-let mes = 'maio';
-let candidato = '?';
+// let mes = 'maio';
+// let candidato = '?';
 
-switch (mes){  
-    case 'janeiro':
-    case 'fevereiro':
-    case 'março':
-    candidato = 'Verão'
-break;
-        case 'abril':
-        case 'maio':
-        case 'junhho':
-        candidato = 'Outono'
-break;
-        case 'julho':
-        case 'agosto':
-        case 'setembro':
-        candidato = 'Inverno'
-break;
-    case 'outubro':
-    case 'novembro':
-    case 'dezembro':
-    canditato = 'Primavera'
+// switch (mes){  
+//     case 'janeiro':
+//     case 'fevereiro':
+//     case 'março':
+//     candidato = 'Verão'
+// break;
+//         case 'abril':
+//         case 'maio':
+//         case 'junhho':
+//         candidato = 'Outono'
+// break;
+//         case 'julho':
+//         case 'agosto':
+//         case 'setembro':
+//         candidato = 'Inverno'
+// break;
+//     case 'outubro':
+//     case 'novembro':
+//     case 'dezembro':
+//     canditato = 'Primavera'
+// }
+// console.log (candidato)
+//
+let pessoaCanditada = 'aprovada';
+switch(pessoaCanditada){
+    case 'aprovada':
+        console.log('Você foi aprovada no processo selêtivo Trybe !')
+        break;
+case 'reprovada':
+    console.log('infelizmente não foi dessa vez :(')
+    break;
+case 'lista':
+    console.log('Você está em nossa lista de espera, fique atento as novidades. :)')
+    break;
+
+    default:
+        console.log("Não se aplica.")
 }
-console.log (candidato)
