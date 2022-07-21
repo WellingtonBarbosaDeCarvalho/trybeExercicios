5c78a734e6a694d59ae853ee6cba5e158fa968a2
Wellington B <barbosadecarvalhow@gmail.com>
2022-07-21 16:09:36 -0300

    adiciona exercicio for/of;

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios-for/exercicio-for.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios-for/exercicio-for.js
@@ -5,3 +5,11 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for(let index = 0; index <= groceryList.length; index +=1){
     console.log(groceryList[index]);
 }
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nam of names){
+    console.log(nam)
+}
